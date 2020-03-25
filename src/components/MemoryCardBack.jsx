cda189a8ec8d773a381483250bad245ca9f4f3b3
MichaelBamba <mjbamba2@gmail.com>
2020-03-25 13:17:03 -0400

    finished p2 full flippablbity

--- src/components/MemoryCardBack.jsx
+++ src/components/MemoryCardBack.jsx
@@ -1,35 +1,39 @@
 import React, { Component } from "react";
 import "./MemoryCardBack.css";
 class MemoryCard extends Component {
-  constructor() {
-    super();
-    this.state = { isFlipped: false };
-  }
-  clickHandler() {
+  state = {
+    isFlipped: false
+  };
+
+  clickHandler = () => {
     this.setState({
       isFlipped: !this.state.isFlipped
     });
-  }
-  render() {
-    let memoryCardInnerClass = "MemoryCardInner";
+  };
 
-    this.state.isFlipped
-      ? (memoryCardInnerClass = memoryCardInnerClass + " flipped")
-      : (memoryCardInnerClass = "MemoryCardInner");
+  render() {
+    const { isFlipped } = this.state;
+
+    const memoryCardInnerClass =
+      "MemoryCardInner " + (isFlipped === true && "flipped");
+
     return (
-      <div class="MemoryCard" onClick="{this.clickHandler}">
-        <div class="MemoryCardInner">
-          <div class="MemoryCardBack">
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className={memoryCardInnerClass}>
+          <div className="MemoryCardBack">
             <img
               src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
               alt="logo"
               class="img"
-            ></img>
+            />
           </div>
-          <div class="MemoryCardFront">∆</div>
+          <div className="MemoryCardFront">
+            <p>∆</p>
+          </div>
         </div>
       </div>
     );
   }
 }
+
 export default MemoryCard;
